29137c928bac61a87b59186dfd8d6da5d7e23a43
angela-wheatley <79345893+angela-wheatley@users.noreply.github.com>
2021-03-24 17:02:40 -0500

    first commit of project

---
+++ src/main/java/com/tts/formvalidation/FormValidationHwApplication.java
@@ -0,0 +1,13 @@
+package com.tts.formvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormValidationHwApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(FormValidationHwApplication.class, args);
+	}
+
+}
